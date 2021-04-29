39733f2b52184507fbed8c473edbea3e0f85b5c3
Ahmed <ahmedchouihi@gmail.com>
2021-04-29 11:05:33 +0100

    'Routes,Controllers,Pages'

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already');
+    }
+
+    /**
+     * @Route ("/news/{slug}")
+     */
+
+    public function show($slug)
+    {
+        return new Response(sprintf('Future page to show one space article %s',
+            $slug));
+    }
+
+}
